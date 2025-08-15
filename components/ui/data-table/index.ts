874b665ee247a94df51f8efe0fd5886acc2d15b4
Owl33 <wianw@naver.com>
2025-08-15 13:26:32 +0900

    data-table component update

--- components/ui/data-table/index.ts
+++ components/ui/data-table/index.ts
@@ -3,25 +3,27 @@ export { DataTable } from "./core";
 export type { DataTableRef } from "./core";
 
 // Export commonly used utilities for external use
-export { createSelectionColumn, createActionsColumn, createCustomActionsColumn } from "./table-columns";
+export {
+  createSelectionColumn,
+  createActionsColumn,
+  createCustomActionsColumn,
+} from "./table-columns";
 export { ColumnManager } from "./table-columns";
 export { CellRenderer } from "./table-layout";
 
 // Export constants and types for customization
-export { 
-  SYSTEM_COLUMN_IDS, 
+export {
+  SYSTEM_COLUMN_IDS,
   DEFAULT_PAGE_SIZE_OPTIONS,
-  COLUMN_PRIORITIES,
-  CELL_RENDERER_TYPES 
+  CELL_RENDERER_TYPES,
 } from "./shared/constants";
 
-export type { 
-  ActionItem, 
+export type {
+  ActionItem,
   DataTableRef as DataTableRefType,
-  ColumnPriority,
   CellRendererType,
   EditState,
-  FilterState 
+  FilterState,
 } from "./shared/types";
 
 // Export hooks for advanced usage
